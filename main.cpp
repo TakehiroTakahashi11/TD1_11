a9272a2bf166f85877a2fb8fe13d2e2905e32cd9
Take <k022g0071@g.neec.ac.jp>
2022-10-29 13:40:57 +0900

    main.cppを簡潔に

--- main.cpp
+++ main.cpp
@@ -1,52 +1,12 @@
 ﻿#include <Novice.h>
+#include "Game.h"
 
-const char kWindowTitle[] = "学籍番号";
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd) {
+	// ゲームクラスをインスタンス化
+	Game game = Game();
 
-// Windowsアプリでのエントリーポイント(main関数)
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+	// ゲームループ
+	game.Run();
 
-	// ライブラリの初期化
-	Novice::Initialize(kWindowTitle, 1280, 720);
-
-	// キー入力結果を受け取る箱
-	char keys[256] = {0};
-	char preKeys[256] = {0};
-
-	// ウィンドウの×ボタンが押されるまでループ
-	while (Novice::ProcessMessage() == 0) {
-		// フレームの開始
-		Novice::BeginFrame();
-
-		// キー入力を受け取る
-		memcpy(preKeys, keys, 256);
-		Novice::GetHitKeyStateAll(keys);
-
-		///
-		/// ↓更新処理ここから
-		///
-
-		///
-		/// ↑更新処理ここまで
-		///
-
-		///
-		/// ↓描画処理ここから
-		///
-
-		///
-		/// ↑描画処理ここまで
-		///
-
-		// フレームの終了
-		Novice::EndFrame();
-
-		// ESCキーが押されたらループを抜ける
-		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
-			break;
-		}
-	}
-
-	// ライブラリの終了
-	Novice::Finalize();
 	return 0;
 }
