3e82cb4b8d0b03629d864cca3a11c750f595d62a
Take <k022g0071@g.neec.ac.jp>
2022-10-31 09:22:15 +0900

    エフェクトマネージャー下書き

--- InGameScene.cpp
+++ InGameScene.cpp
@@ -4,6 +4,7 @@
 #include "Quad.h"
 #include "KeyMouseInput.h"
 #include "Game.h"
+#include "EfffectManager.h"
 
 InGameScene::InGameScene(Game& pGame) : BaseScene(pGame)
 {
@@ -11,13 +12,20 @@ InGameScene::InGameScene(Game& pGame) : BaseScene(pGame)
 
 void InGameScene::Init() {
 	getCameraMain().setScale(1.0f);
+	// EffectManager::MakeNewEffect({ 100.0f,100.0f }, Click);
+
+	// èâä˙âªÇî≤ÇØÇÈ
 	getGame().ChangePhase(Game::kUpdate);
 }
+
 void InGameScene::Update() {
+	
 }
+
 void InGameScene::Draw() {
 	getCameraUI().DrawQuad({ {0,0},1920,1080 }, Datas::BackGroundTex,0,0,BLACK);
 }
+
 void InGameScene::Finalise() {
 
 }
