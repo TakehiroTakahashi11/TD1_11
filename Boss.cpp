6c8cac7507ea4bdecf1e26a7b3e4c414f0239ae1
Take <k022g0071@g.neec.ac.jp>
2022-11-01 18:07:38 +0900

    ボスクラス基盤

---
+++ Boss.cpp
@@ -0,0 +1,24 @@
+#include "Boss.h"
+#include "Datas.h"
+#include "Camera.h"
+
+Boss::Boss(Game& pGame) : Obj(pGame)
+{
+	Init();
+}
+
+void Boss::Init()
+{
+	position = { Datas::BOSS_POS_X,Datas::BOSS_POS_Y };
+	width = Datas::BOSS_WIDTH;
+	height = Datas::BOSS_HEIGHT;
+}
+
+void Boss::Update()
+{
+}
+
+void Boss::Draw()
+{
+	getCameraMain().DrawQuad({ {position.x - width * 0.5f,position.y - height * 0.5f},width,height }, Datas::BOSS_TEX);
+}
---
+++ Boss.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Obj.h"
+
+class Boss : public Obj
+{
+public:
+	/// @brief コンストラクタ
+	/// @param pGame ゲームのポインタ
+	Boss(class Game& pGame);
+
+	/// @brief 初期化処理
+	void Init() override;
+
+	/// @brief 更新処理
+	void Update() override;
+
+	/// @brief 描画処理
+	void Draw() override;
+
+private:
+
+private:
+	
+};
