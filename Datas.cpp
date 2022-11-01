ff5d55d5dbfd559f8fa1c28839d14ade9f794192
Take <k022g0071@g.neec.ac.jp>
2022-11-01 18:09:57 +0900

    Player,bossのマジナン達を追加

--- Datas.cpp
+++ Datas.cpp
@@ -2,11 +2,17 @@
 #include <Novice.h>
 #include "KeyMouseInput.h"
 
+Texture Datas::PLAYER_TEX = Texture(PLAYER_WIDTH, PLAYER_HEIGHT);
+Texture Datas::BOSS_TEX = Texture(BOSS_WIDTH, BOSS_HEIGHT);
 Texture Datas::MouseTex = Texture(16, 16);
 Texture Datas::BackGroundTex = Texture(1920, 1080);
 
 void Datas::LoadTexture()
 {
+	PLAYER_TEX.SetHandle(Novice::LoadTexture("white1x1.png"));
+	Per++;
+	BOSS_TEX.SetHandle(Novice::LoadTexture("white1x1.png"));
+	Per++;
 	MouseTex.SetHandle(Novice::LoadTexture("white1x1.png"));
 	Per++;
 	BackGroundTex.SetHandle(Novice::LoadTexture("white1x1.png"));
@@ -15,5 +21,5 @@ void Datas::LoadTexture()
 }
 
 int Datas::Per = 0;
-const int Datas::Max_Per = 2;
+const int Datas::Max_Per = 4;
 bool Datas::IsEnd = false;
--- Datas.h
+++ Datas.h
@@ -1,10 +1,12 @@
 #pragma once
 #include "Texture.h"
 #include "Sound.h"
+#include "Vector2D.h"
 
 class Datas
 {
 public:
+    // =========================
     // WINDOW
 
     static constexpr char WINDOW_TITLE[] = "11ŒŽTD1";
@@ -18,12 +20,31 @@ public:
     static constexpr float SCREEN_HALF_HEIGHT = SCREEN_HEIGHT * 0.5f;
 
     // =========================
+    // PLAYER
+    static constexpr float PLAYER_POS_X = Datas::SCREEN_HALF_WIDTH;
+    static constexpr float PLAYER_POS_Y = 0.0f;
+    static constexpr int PLAYER_WIDTH = 25;
+    static constexpr int PLAYER_HEIGHT = 25;
+    static constexpr int PLAYER_SPEED = 20;
+    static constexpr int PLAYER_DASH_SPEED = 40;
+    static Texture PLAYER_TEX;
+
+    // =========================
+    // BOSS
+    static constexpr float BOSS_POS_X = Datas::SCREEN_HALF_WIDTH;
+    static constexpr float BOSS_POS_Y = 500.0f;
+    static constexpr int BOSS_WIDTH = 250;
+    static constexpr int BOSS_HEIGHT = 250;
+    static Texture BOSS_TEX;
+
+    // =========================
     // Texture
 
     static Texture MouseTex;
     static Texture BackGroundTex;
 
-public:
+    // =========================
+public:// Load
     static void LoadTexture();
     static int GetPer() { return Per; }
     static bool GetIsEnd() { return IsEnd; }
