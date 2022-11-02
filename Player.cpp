a714d1abe3f9fc8ee5eff424f618f5022cfc9f58
Take <k022g0071@g.neec.ac.jp>
2022-11-02 18:27:38 +0900

    プレイヤー回転描画

--- Player.cpp
+++ Player.cpp
@@ -7,8 +7,9 @@
 #include "Delta.h"
 #include "Novice.h"
 #include "Gauntlets.h"
+#include "MyFunc.h"
 #define _USE_MATH_DEFINES
-#include <math.h>
+#include <cmath>
 
 /// @brief コンストラクタ
 /// @param pGame ゲームのポインタ
@@ -51,6 +52,10 @@ void Player::Update() {
 		enum_direction = DirectionConv();// enumに変換して保存
 	}
 
+	// カメラ追尾
+	 getCameraMain().setPosition(position);
+	 getCameraMain().setScale(2.0f);
+
 	getGauntlets().Update();// ガントレットの更新処理
 
 	if (Datas::DEBUG_MODE) {// デバッグ用文字列
@@ -72,51 +77,12 @@ void Player::Update() {
 
 /// @brief 描画処理
 void Player::Draw() {
-	if (!getGauntlets().GetIsBreak() && direction.x < 0) {// プレイヤーが左向きならば
-		getGauntlets().Draw();// プレイヤーより後に描画
-
-		if (Datas::DEBUG_MODE) {// デバッグ用文字列
-			Novice::ScreenPrintf(0, 140, "gauntlet_wo_ushiro_ni_byouga");
-		}
-	}
-
 	// プレイヤー描画
-	switch (enum_direction)
-	{
-	case Player::kDown:
-		getCameraMain().DrawQuad({ {position.x - width * 0.5f,position.y - height * 0.5f}, width, height }, Datas::PLAYER_DOWN_TEX, move_anim / Datas::PLAYER_ANIM_SPD);
-		break;
-	case Player::kR:
-		getCameraMain().DrawQuad({ {position.x - width * 0.5f,position.y - height * 0.5f}, width, height }, Datas::PLAYER_RIGHT_TEX, move_anim / Datas::PLAYER_ANIM_SPD);
-		break;
-	case Player::kL:
-		getCameraMain().DrawQuad({ {position.x - width * 0.5f,position.y - height * 0.5f}, width, height }, Datas::PLAYER_LEFT_TEX, move_anim / Datas::PLAYER_ANIM_SPD);
-		break;
-	case Player::kDownR:
-		getCameraMain().DrawQuad({ {position.x - width * 0.5f,position.y - height * 0.5f}, width, height }, Datas::PLAYER_DOWN_RIGHT_TEX, move_anim / Datas::PLAYER_ANIM_SPD);
-		break;
-	case Player::kDownL:
-		getCameraMain().DrawQuad({ {position.x - width * 0.5f,position.y - height * 0.5f}, width, height }, Datas::PLAYER_DOWN_LEFT_TEX, move_anim / Datas::PLAYER_ANIM_SPD);
-		break;
-	case Player::kUpR:
-		getCameraMain().DrawQuad({ {position.x - width * 0.5f,position.y - height * 0.5f}, width, height }, Datas::PLAYER_UP_RIGHT_TEX, move_anim / Datas::PLAYER_ANIM_SPD);
-		break;
-	case Player::kUpL:
-		getCameraMain().DrawQuad({ {position.x - width * 0.5f,position.y - height * 0.5f}, width, height }, Datas::PLAYER_UP_LEFT_TEX, move_anim / Datas::PLAYER_ANIM_SPD);
-		break;
-	default:
-	case Player::kUp:
-		getCameraMain().DrawQuad({ {position.x - width * 0.5f,position.y - height * 0.5f}, width, height }, Datas::PLAYER_UP_TEX,move_anim / Datas::PLAYER_ANIM_SPD);
-		break;
-	}
+	Quad temp = My::RotateCenter(position, atan2f(direction.y, direction.x), width, height);// 回転
+	getCameraMain().DrawQuad(temp, Datas::PLAYER_TEX, move_anim / Datas::PLAYER_ANIM_SPD);// 実際に描画
 
-	if (!getGauntlets().GetIsBreak() && 0 <= direction.x) {// プレイヤーが右向きならば
-		getGauntlets().Draw();// プレイヤーより前に描画
-
-		if (Datas::DEBUG_MODE) {// デバッグ用文字列
-			Novice::ScreenPrintf(0, 140, "gauntlet_wo_mae_ni_byouga");
-		}
-	}
+	// ガントレット描画
+	getGauntlets().Draw();
 }
 
 void Player::Move()
@@ -155,7 +121,7 @@ void Player::Move()
 		move_anim = 0.0f;
 	}
 
-	if (3.0f * Datas::PLAYER_ANIM_SPD < move_anim) {// 最大コマ
+	if (4.0f * Datas::PLAYER_ANIM_SPD < move_anim) {// 最大コマ
 		move_anim = 0.0f;
 	}
 }
@@ -214,66 +180,57 @@ void Player::Guard() {
 
 Player::DirectionEnum Player::DirectionConv()
 {
-	Vector2D vec[4];
-	float rad;
-	for (int i = 0; i < 4; i++) {
-		rad = ((22.5 + i * 45) * M_PI) / 180;
-		vec[i].x = cosf(rad);
-		vec[i].y = sinf(rad);
-	}
-	float dot_result[8];
-	for (int i = 0; i < 4; i++) {
-		dot_result[i] = vec[i].Dot(direction);
-		dot_result[i + 4] = -dot_result[i];
-	}
-
-	if (dot_result[0] > 0 && dot_result[3] >= 0){
+	switch (My::VectorDirection8(direction))
+	{
+	default:
+	case 0:
 		if (Datas::DEBUG_MODE) {
-			Novice::ScreenPrintf(270, 80, "Up");
+			Novice::ScreenPrintf(300, 80, "Up");
 		}
 		return DirectionEnum::kUp;
-	}
-	if (dot_result[1] > 0 && dot_result[4] >= 0) {
+		break;
+	case 1:
 		if (Datas::DEBUG_MODE) {
-			Novice::ScreenPrintf(270, 80, "UpLeft");
+			Novice::ScreenPrintf(300, 80, "UpLeft");
 		}
 		return DirectionEnum::kUpL;
-	}
-	if (dot_result[2] > 0 && dot_result[5] >= 0) {
+		break;
+	case 2:
 		if (Datas::DEBUG_MODE) {
-			Novice::ScreenPrintf(270, 80, "Left");
+			Novice::ScreenPrintf(300, 80, "Left");
 		}
 		return DirectionEnum::kL;
-	}
-	if (dot_result[3] > 0 && dot_result[6] >= 0) {
+		break;
+	case 3:
 		if (Datas::DEBUG_MODE) {
-			Novice::ScreenPrintf(270, 80, "DownLeft");
+			Novice::ScreenPrintf(300, 80, "DownLeft");
 		}
 		return DirectionEnum::kDownL;
-	}
-	if (dot_result[4] > 0 && dot_result[7] >= 0) {
+		break;
+	case 4:
 		if (Datas::DEBUG_MODE) {
-			Novice::ScreenPrintf(270, 80, "Down");
+			Novice::ScreenPrintf(300, 80, "Down");
 		}
 		return DirectionEnum::kDown;
-	}
-	if (dot_result[5] > 0 && dot_result[0] >= 0) {
+		break;
+	case 5:
 		if (Datas::DEBUG_MODE) {
-			Novice::ScreenPrintf(270, 80, "DownRight");
+			Novice::ScreenPrintf(300, 80, "DownRight");
 		}
 		return DirectionEnum::kDownR;
-	}
-	if (dot_result[6] > 0 && dot_result[1] >= 0) {
+		break;
+	case 6:
 		if (Datas::DEBUG_MODE) {
-			Novice::ScreenPrintf(270, 80, "Right");
+			Novice::ScreenPrintf(300, 80, "Right");
 		}
 		return DirectionEnum::kR;
-	}
-	if (dot_result[7] > 0 && dot_result[2] >= 0) {
+		break;
+	case 7:
 		if (Datas::DEBUG_MODE) {
-			Novice::ScreenPrintf(270, 80, "UpRight");
+			Novice::ScreenPrintf(300, 80, "UpRight");
 		}
 		return DirectionEnum::kUpR;
+		break;
 	}
 
 	return DirectionEnum::kUp;
