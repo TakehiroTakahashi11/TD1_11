a9d2f37eb60c243505e136e89a82bd108f589a69
Take <k022g0071@g.neec.ac.jp>
2022-11-05 19:26:49 +0900

    初期化修正

--- Thunder.cpp
+++ Thunder.cpp
@@ -8,6 +8,7 @@
 #include "Game.h"
 #include "Player.h"
 #include "Datas.h"
+#include <Novice.h>
 
 Thunder::Thunder(Game& game, Vector2D pos) : BaseEffect(game, pos)
 {
@@ -20,18 +21,22 @@ Thunder::~Thunder()
 
 void Thunder::Init()
 {
-	col_pos = { position.x + 255.0f, position.y + 45.0f };
+	elapsedFrame = 0.0f;
+	col_pos = { position.x + 145.0f, position.y + 45.0f };
 }
 
 void Thunder::Update()
 {
+	Vector2D temp = { Datas::PLAYER_WIDTH * 1.9f,-Datas::PLAYER_WIDTH * 0.5f };
 	// ìñÇΩÇËîªíË
 	if (!pGame.getPlayer().GetIsDash() && !pGame.getPlayer().GetIsInvincible()) {
-		
+		if (My::CollisonCircletoPoint(col_pos, Datas::EFFECTS_THUNDER_SIZE_X, Datas::EFFECTS_THUNDER_SIZE_Y, pGame.getPlayer().GetPosition() + temp)) {
+			pGame.getPlayer().SetDamage(Datas::BOSS_THUNDER1_DAMAGE);
+		}
 	}
 
 	// èIóπèàóù
-	if (30.0f < elapsedFrame) {
+	if (40.0f < elapsedFrame) {
 		isEnd = true;
 	}
 }
@@ -39,7 +44,7 @@ void Thunder::Update()
 void Thunder::Draw()
 {
 	if (!isEnd) {
-		getCameraMain().DrawQuad({ {position.x - 255.0f,position.y - 45.0f} ,Datas::EFFECTS_THUNDER_SIZE ,Datas::EFFECTS_THUNDER_SIZE }, Datas::EFFECTS_THUNDER_TEX,
+		getCameraMain().DrawQuad({ {position.x - 145.0f,position.y - 45.0f} ,Datas::EFFECTS_THUNDER_SIZE ,Datas::EFFECTS_THUNDER_SIZE }, Datas::EFFECTS_THUNDER_TEX,
 			static_cast<int>(elapsedFrame) % 7, static_cast<int>(elapsedFrame) / 7);
 	}
 }
